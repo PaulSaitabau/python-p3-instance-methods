4d7c479a224543d1264b47aa970b0bc80fdec81e
paulsaitabau <paulsaitabau05@gmail.com>
2024-05-25 13:43:57 +0300

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -3,5 +3,12 @@
 class Dog:
     # Class body goes here
 
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
     #Instance method definition
-    pass
+if __name__ == "__main__":
+    my_dog = Dog()
+    my_dog.sit()  # This will output: The dog is sitting.
